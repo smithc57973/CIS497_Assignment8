dbfa5780a913d838bf666a9a2efc7a616b46a715
smithc57973 <44327733+smithc57973@users.noreply.github.com>
2020-11-11 12:32:08 -0600

    Finished Prototype 5 Follow Along

---
+++ Prototype5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    private int score;
+    public bool isGameActive;
+    public GameObject titleScreen;
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            //wait 1 second
+            yield return new WaitForSeconds(spawnRate);
+
+            //generate random index and spawn target based on index
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(score);
+    }
+}
